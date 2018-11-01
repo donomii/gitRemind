b113b3b02c3f3de22e2709e58af8a66b582ca6f5
Jeremy Price <jeremy.price@careem.com>
2018-11-01 22:25:23 +0100

    Cleanup, add sync channel for finish scan

--- main.go
+++ main.go
@@ -1,206 +1,146 @@
 package main
 
 import (
-    "github.com/rivo/tview"
-    "flag"
-    "regexp"
-    "strings"
-    "fmt"
-    "path/filepath"
-    "crypto/md5"
-    "encoding/hex"
-    "io"
-    "os"
-    "os/exec"
-    "bytes"
-)
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
 
+	"github.com/donomii/goof"
+	"github.com/rivo/tview"
+)
 
 var autoSync bool
 var ui bool
+var verbose bool
 var repos [][]string
-var lastSelect  string
+var lastSelect string
 var app *tview.Application
 var workerChan chan string
+var doneChan chan bool
 
-func hash_file_md5(filePath string) (string, error) {
-    //Initialize variable returnMD5String now in case an error has to be returned
-    var returnMD5String string
-
-    //Open the passed argument and check for any error
-    file, err := os.Open(filePath)
-    if err != nil {
-        return returnMD5String, err
-    }
-
-    //Tell the program to call the following function when the current function returns
-    defer file.Close()
-
-    //Open a new hash interface to write to
-    hash := md5.New()
-
-    //Copy the file in the hash interface and check for any error
-    if _, err := io.Copy(hash, file); err != nil {
-        return returnMD5String, err
-    }
-
-    //Get the 16 bytes hash
-    hashInBytes := hash.Sum(nil)[:16]
-
-    //Convert the bytes to a string
-    returnMD5String = hex.EncodeToString(hashInBytes)
-
-    return returnMD5String, nil
-
-}
-
-func quickCommand (cmd *exec.Cmd) string{
-    in := strings.NewReader("")
-    cmd.Stdin = in
-    var out bytes.Buffer
-    cmd.Stdout = &out
-    var err bytes.Buffer
-    cmd.Stderr = &err
-    cmd.Run()
-    //fmt.Printf("Command result: %v\n", res)
-    ret := fmt.Sprintf("%s", out)
-    //fmt.Println(ret)
-    return ret
-}
-
-
-func doQC (strs []string) string {
-	cmd := exec.Command(strs[0], strs[1:]...)
-    return quickCommand(cmd)
-}
-
-func quickCommandInteractive (cmd *exec.Cmd) {
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    cmd.Run()
-}
-
-
-
-func doQCI (strs []string) {
-	cmd := exec.Command(strs[0], strs[1:]...)
-                quickCommandInteractive(cmd)
-}
-
-func worker (c chan string) {
-    var ahead_regex = regexp.MustCompile(`Your branch is ahead of`)
-    var not_staged_regex = regexp.MustCompile(`Changes not staged for commit:`)
-    var staged_not_committed_regex = regexp.MustCompile(`Changes to be committed`)
-    var modified_regex = regexp.MustCompile(`modified:`)
-    var untracked_regex = regexp.MustCompile(`Untracked files:`)
+func worker(c chan string) {
+	var ahead_regex = regexp.MustCompile(`Your branch is ahead of`)
+	var not_staged_regex = regexp.MustCompile(`Changes not staged for commit:`)
+	var staged_not_committed_regex = regexp.MustCompile(`Changes to be committed`)
+	var modified_regex = regexp.MustCompile(`modified:`)
+	var untracked_regex = regexp.MustCompile(`Untracked files:`)
 	repos = [][]string{}
-        cwd, _ := os.Getwd()
-        for path := range c {
-            os.Chdir(cwd)
-            if f, _ := os.Stat(fmt.Sprintf("%v/%v", path, ".git")); f != nil && f.IsDir() {
-                os.Chdir(path)
-                cmd := exec.Command("git", "status")
-                result := quickCommand(cmd)
-				cmd = exec.Command("git", "status", "--porcelain")
-                shortresult := quickCommand(cmd)
-				cmd = exec.Command("git", "diff", "--ignore-blank-lines")
-                diffresult := quickCommand(cmd)
-				reasons := []string{}
-				longreasons := []string{}
-                if ahead_regex.MatchString(result) {
-					reasons = append(reasons, "!push")
-					longreasons = append(longreasons, "local commits not pushed")
-				}
-				if modified_regex.MatchString(result) || not_staged_regex.MatchString(result) || staged_not_committed_regex.MatchString(result) {
-					reasons = append(reasons, "!commtd")
-					longreasons = append(longreasons, "changes not committed")
-				}
-				if untracked_regex.MatchString(result) {
-					reasons = append(reasons, "!tracked")
-					longreasons = append(longreasons, "untracked files present")
-				}
-				if len(reasons)>0 {
-                    fmt.Printf("%v: %v\n", path, strings.Join(longreasons, ", "))
-                    repos = append(repos, []string{path, shortresult, grep(diffresult), strings.Join(reasons, ", "), strings.Join(longreasons, ", ")})
-                    //fmt.Println(result)
-                    //fmt.Printf("\n\n\n\n\n")
-                }
-                if autoSync  {
-                    fmt.Println("Syncing "+path)
-                    cmd := exec.Command("git", "push")
-                    quickCommand(cmd)
-                    cmd = exec.Command("git", "pull")
-                    quickCommand(cmd)
-                }
-            }
+	cwd, _ := os.Getwd()
+	for path := range c {
+		if verbose {
+            log.Println(path)
         }
+		os.Chdir(cwd)
+		if f, _ := os.Stat(fmt.Sprintf("%v/%v", path, ".git")); f != nil && f.IsDir() {
+			os.Chdir(path)
+			cmd := exec.Command("git", "status")
+			result := goof.QuickCommand(cmd)
+			cmd = exec.Command("git", "status", "--porcelain")
+			shortresult := goof.QuickCommand(cmd)
+			cmd = exec.Command("git", "diff", "--ignore-blank-lines")
+			diffresult := goof.QuickCommand(cmd)
+			reasons := []string{}
+			longreasons := []string{}
+			if ahead_regex.MatchString(result) {
+				reasons = append(reasons, "!push")
+				longreasons = append(longreasons, "local commits not pushed")
+			}
+			if modified_regex.MatchString(result) || not_staged_regex.MatchString(result) || staged_not_committed_regex.MatchString(result) {
+				reasons = append(reasons, "!commtd")
+				longreasons = append(longreasons, "changes not committed")
+			}
+			if untracked_regex.MatchString(result) {
+				reasons = append(reasons, "!tracked")
+				longreasons = append(longreasons, "untracked files present")
+			}
+			if len(reasons) > 0 {
+				fmt.Printf("%v: %v\n", path, strings.Join(longreasons, ", "))
+				repos = append(repos, []string{path, shortresult, grep(diffresult), strings.Join(reasons, ", "), strings.Join(longreasons, ", ")})
+                if verbose {
+				fmt.Println(result)
+				fmt.Printf("\n\n\n\n\n")
+			}
+        }
+			if autoSync {
+				fmt.Println("Syncing " + path)
+				cmd := exec.Command("git", "push")
+				goof.QuickCommand(cmd)
+				cmd = exec.Command("git", "pull")
+				goof.QuickCommand(cmd)
+			}
+		}
+	}
+    doneChan <- true
 }
 
-func grep (str string) string {
+func grep(str string) string {
 	var out string
 	strs := strings.Split(str, "\n")
 	for _, v := range strs {
-		if strings.Index( v, "+") == 0 || strings.Index( v, "-") == 0  {
+		if strings.Index(v, "+") == 0 || strings.Index(v, "-") == 0 {
 			out = out + v + "\n"
 		}
-	}	
+	}
 	return out
 }
 
 func doui() {
 
-        
-    //box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-    app = tview.NewApplication()
-    	textView := tview.NewTextView().
+	//box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+	app = tview.NewApplication()
+	textView := tview.NewTextView().
 		SetDynamicColors(true).
 		SetRegions(true).
 		SetWordWrap(true).
 		SetChangedFunc(func() {
 			app.Draw()
 		})
-        textView.SetText( "lalalala")
-		
-		textView2 := tview.NewTextView().
+	textView.SetText("lalalala")
+
+	textView2 := tview.NewTextView().
 		SetDynamicColors(true).
 		SetRegions(true).
 		SetWordWrap(true).
 		SetChangedFunc(func() {
 			app.Draw()
 		})
-        textView2.SetText( "lalalala")
-		
-		footer := tview.NewTextView().
+	textView2.SetText("lalalala")
+
+	footer := tview.NewTextView().
 		SetDynamicColors(true).
 		SetRegions(true).
 		SetWordWrap(true).
 		SetChangedFunc(func() {
 			app.Draw()
 		})
-        footer.SetText( "lalalala")
-		
-		newPrimitive := func(text string) tview.Primitive {
-			return tview.NewTextView().
+	footer.SetText("lalalala")
+
+	newPrimitive := func(text string) tview.Primitive {
+		return tview.NewTextView().
 			SetTextAlign(tview.AlignCenter).
 			SetText(text)
-		}
-		
+	}
+
 	list := tview.NewList()
-        for i, vv := range repos {
-			
-            ii := i
-			v := vv
-            list.AddItem(v[0], v[3], 'a', func(){
-			
+	for i, vv := range repos {
+
+		ii := i
+		v := vv
+		list.AddItem(v[0], v[3], 'a', func() {
+
 			if lastSelect == v[0] {
 				app.Stop()
-				        cwd, _ := os.Getwd()
-        
+				cwd, _ := os.Getwd()
+
 				os.Chdir(v[0])
 				fmt.Printf("%v\n", []string{"git", "commit", "-a"})
-				doQCI([]string{"git", "commit", "-a"})
-				doQCI([]string{"git", "push"})
+				goof.QCI([]string{"git", "commit", "-a"})
+				goof.QCI([]string{"git", "push"})
 				os.Chdir(cwd)
 				scanRepos(workerChan)
 				app.Run()
@@ -209,65 +149,68 @@ func doui() {
 			textView2.SetText(repos[ii][1])
 			footer.SetText(repos[ii][4])
 			lastSelect = v[0]
-			})
-        }
-		list.AddItem("Quit", "Press to exit", 'q', func() {
-			app.Stop()
 		})
+	}
+	list.AddItem("Quit", "Press to exit", 'q', func() {
+		app.Stop()
+	})
 
+	//menu := newPrimitive("Menu")
+	//sideBar := newPrimitive("Side Bar")
 
-        	//menu := newPrimitive("Menu")
-            	//sideBar := newPrimitive("Side Bar")
-
-    grid := tview.NewGrid().
+	grid := tview.NewGrid().
 		SetRows(3, 0, 3).
 		SetColumns(30, 0, 30).
 		SetBorders(true).
 		AddItem(newPrimitive("Header"), 0, 0, 1, 3, 0, 0, false).
 		AddItem(footer, 2, 0, 1, 3, 0, 0, false)
 
-        /*
-        grid.AddItem(menu, 0, 0, 1, 3, 0, 0, false).
-        AddItem(list, 1, 0, 1, 3, 0, 0, true).
-		AddItem(sideBar, 0, 0, 1, 3, 0, 0, false)
-        */
+	/*
+		        grid.AddItem(menu, 0, 0, 1, 3, 0, 0, false).
+		        AddItem(list, 1, 0, 1, 3, 0, 0, true).
+				AddItem(sideBar, 0, 0, 1, 3, 0, 0, false)
+	*/
 
-        grid.AddItem(list, 1, 0, 1, 1, 0, 100, true).
+	grid.AddItem(list, 1, 0, 1, 1, 0, 100, true).
 		AddItem(textView, 1, 1, 1, 1, 0, 100, false).
 		AddItem(textView2, 1, 2, 1, 1, 0, 100, false)
-        //left := flex.AddItem(tview.NewBox().SetBorder(true).SetTitle("Left (1/2 x width of Top)"), 0, 1, false)
-        //row := tview.NewFlex().SetDirection(tview.FlexRow)
-          //row = row.AddItem(list.SetBorder(true).SetTitle("Repos"), 0, 3, true)
-         //row = row.AddItem(textView.SetBorder(true).SetTitle("Status"), 0, 3, false)
-         //flex.AddItem(left.SetBorder(true), 10, 1, false)
+	//left := flex.AddItem(tview.NewBox().SetBorder(true).SetTitle("Left (1/2 x width of Top)"), 0, 1, false)
+	//row := tview.NewFlex().SetDirection(tview.FlexRow)
+	//row = row.AddItem(list.SetBorder(true).SetTitle("Repos"), 0, 3, true)
+	//row = row.AddItem(textView.SetBorder(true).SetTitle("Status"), 0, 3, false)
+	//flex.AddItem(left.SetBorder(true), 10, 1, false)
 
-
-
-
-
-        if err := app.SetRoot(grid, true).Run(); err != nil {
+	if err := app.SetRoot(grid, true).Run(); err != nil {
 		panic(err)
 	}
 }
 
 func scanRepos(c chan string) {
-    walkHandler := func (path string, info os.FileInfo, err error) error {
-        c <- path
-        return nil
-    }
-    //fmt.Println("These repositories need some attention:")
-    filepath.Walk(".", walkHandler)
+	walkHandler := func(path string, info os.FileInfo, err error) error {
+		//log.Println(path)
+		c <- path
+		return nil
+	}
+	//fmt.Println("These repositories need some attention:")
+	filepath.Walk(".", walkHandler)
+    close(c)
 }
 
-func main () {
-    flag.BoolVar(&autoSync, "auto-sync", false, "Automatically push then pull on clean repositories")
-    flag.BoolVar(&ui, "ui", false, "Experimental graphical user interface")
-    flag.Parse()
-    
-	workerChan = make(chan string)
-    go worker (workerChan)
+func main() {
+	flag.BoolVar(&autoSync, "auto-sync", false, "Automatically push then pull on clean repositories")
+	flag.BoolVar(&ui, "ui", false, "Experimental graphical user interface")
+	flag.BoolVar(&verbose, "verbose", false, "Print details while working")
+	flag.Parse()
+
+	workerChan = make(chan string, 10)
+	doneChan = make(chan bool)
+	go worker(workerChan)
 	scanRepos(workerChan)
-	
-    if ui { doui() }
-    fmt.Println("Done!")
+    <- doneChan
+	log.Println("Scan complete!")
+
+	if ui {
+		doui()
+	}
+	fmt.Println("Done!")
 }
